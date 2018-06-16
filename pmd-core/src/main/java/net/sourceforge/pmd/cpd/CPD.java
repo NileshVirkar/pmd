000100e57c0e9b9bd425f91f415da3058d1cce51
NileshVirkar <nilesh.virkar@acellere.com>
2018-06-16 15:06:04 +0530

    commented logic of reverting tokens if lexical error found in file

--- pmd-core/src/main/java/net/sourceforge/pmd/cpd/CPD.java
+++ pmd-core/src/main/java/net/sourceforge/pmd/cpd/CPD.java
@@ -143,13 +143,13 @@ public class CPD {
     }
 
     private void addAndSkipLexicalErrors(SourceCode sourceCode) throws IOException {
-        TokenEntry.State savedTokenEntry = new TokenEntry.State(tokens.getTokens());
+        //TokenEntry.State savedTokenEntry = new TokenEntry.State(tokens.getTokens());
         try {
             addAndThrowLexicalError(sourceCode);
         } catch (TokenMgrError e) {
             System.err.println("Skipping " + sourceCode.getFileName() + ". Reason: " + e.getMessage());
-            tokens.getTokens().clear();
-            tokens.getTokens().addAll(savedTokenEntry.restore());
+            //tokens.getTokens().clear();
+            //tokens.getTokens().addAll(savedTokenEntry.restore());
         }
     }
 
